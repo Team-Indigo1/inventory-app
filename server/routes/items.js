cc58251dc62f3f9dc94cbc159ea3ce7df8df982f
grasberk <ganesh.avula123@gmail.com>
2022-11-04 14:57:08 -0500

    items route

---
+++ server/routes/items.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {Items} = require("../models/index");
+const {check, validationResult} = require('express-validator');
+
+
+const router = express.Router();
+
+//include middleware for parsing the body included into the request
+router.use(express.json());
+router.use(express.urlencoded({extended: true}));
+
+
+
+
+//get all items
+router.get("/", async (req, res) => {
+    const items = await Items.findAll();
+    res.json({items});
+})
+
+//get a specific item
+router.get("/:id", async (req, res) => {
+    const item = await Items.findByPk(req.params.id);
+    res.json({item});
+})
+
+
+module.exports=router;
