02ee9ec95180728739dee070e7eb6efdad2dbc96
Molik Kidd <mzkidd5@gmail.com>
2022-11-06 15:29:24 -0500

    add go back button to card

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Stack from 'react-bootstrap/Stack';
 
 export const Item = (props) => {
 
-  return <div >
+  return <div className='item'>
      <Card style={{ width: '12rem' }}>
-      <Card.Img variant="top" src={props.item.image} />
+      <Card.Img variant="top" src={props.item.image} style={{ width: '150px', height: '150px', }} />
       <Card.Body>
         <Card.Title>{props.item.name}</Card.Title>
         <Card.Text>
-        {props.item.description}
+        {props.item.description.substring(0,100)+"..."}
         </Card.Text>
         <Card.Text>
         {props.item.price}
         </Card.Text>
-        <Button variant="primary">Track Item</Button>
+        <Stack gap={2}>
+        <Button variant="primary" >Track Item</Button>
         <Button variant="primary">Go Back</Button>
+        </Stack>
+        
       </Card.Body>
     </Card>
     </div>
