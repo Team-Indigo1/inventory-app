247604aee310c6cf580e63a1d696eec8a6aeada0
Molik Kidd <mzkidd5@gmail.com>
2022-11-06 16:23:35 -0500

    show one item on page

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Stack from 'react-bootstrap/Stack';
 
 export const Item = (props) => {
 
-  return <div className='item'>
-     <Card style={{ width: '12rem' }}>
-      <Card.Img variant="top" src={props.item.image} style={{ width: '150px', height: '150px', }} />
+  console.log('item props', props)
+  return <div className='item' onClick={() =>props.clickThis(props.item.id)}>
+     <Card style={{ width: '12rem' }} >
+      <Card.Img variant="top" src={props.item.image} style={{ width: '150px', height: '150px', margin: "20px" }} />
       <Card.Body>
         <Card.Title>{props.item.name}</Card.Title>
         <Card.Text>
         {props.item.description.substring(0,100)+"..."}
         </Card.Text>
         <Card.Text>
-        {props.item.price}
+        ${props.item.price}
         </Card.Text>
-        <Stack gap={2}>
-        <Button variant="primary" >Track Item</Button>
-        <Button variant="primary">Go Back</Button>
-        </Stack>
-        
+       
       </Card.Body>
     </Card>
     </div>
--- public/react/components/ItemList.js
+++ public/react/components/ItemList.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import apiURL from '../api';
 import {Item} from './Item';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 
 export const ItemsList = ({items}) => {
+
+	const [oneItem, setOneItem] = useState();
+
+	async function getItemsId(id) {
+		const res = await fetch(`${apiURL}/items/${id}`);
+		const data = await res.json();
+		setOneItem(data);
+		console.log("id from item", id)
+	}
+
 	return <div className='d-flex flex-wrap justify-content-between'>
-		{
-			items.map((item, idx) => {
-				return <Item item={item} key={idx} />
-			})
+		{oneItem ? 
+		<div class="container">
+		<div class="row justify-content-md-center">
+		  <div class="col-5" >
+			<div id='description'>
+				<h2>Category: {oneItem.category}</h2>
+				<h6>{oneItem.description}</h6>
+				<h3>${oneItem.price}</h3>
+			</div>
+			<Stack gap={2}>
+			<Button style={{ width:'200px'}}>Go Back</Button>
+			<Button style={{ width:'200px'}}>Add to Cart</Button>
+        	</Stack>
+		  </div>
+		  <div class="col">
+			<img src={oneItem.image} style={{ height:"400px", padding:"20px"}}/>
+		  </div>
+		</div>
+		</div>
+		:
+		items.map((item, idx) => {
+			return <Item item={item} key={idx} clickThis={getItemsId} />
+		})
 		}
 	</div>
 } 
