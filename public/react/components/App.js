03e7cfc446be8d0d277ecb8af878abf8b1991de4
Molik Kidd <mzkidd5@gmail.com>
2022-11-07 12:24:49 -0500

    update container around all items

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -34,7 +34,9 @@ export const App = () => {
 			{ userEmail ?
 			<div > { items ? 
 				<div className="listContainer">
+					<div className='listSection'>
 					<ItemsList items={items} setItems={setItems}/>
+					</div>
 				</div>
 				:
 				<div className='allOrSearch'>
